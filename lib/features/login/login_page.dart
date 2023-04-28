e1206c1f21e5055bdff3007ed2e2ae7a9262c3dd
mustafa oge <daraheni06@gmail.com>
2023-04-28 10:23:04 +0300

    login Page updated

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -32,60 +32,73 @@ class _LoginViewState extends ConsumerState<LoginView> {
           padding: context.paddingMedium,
           child: Column(
             children: [
-              Text(ref.watch(loginProvider).toString()),
-              Column(
-                children: [
-                  TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Firma Id',
-                      hintText: '',
-                    ),
-                  ),
-                  TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Parola',
-                      hintText: '',
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => LoginView()));
-                  },
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: context.dynamicHeight(0.1),
-                    child: Center(
-                      child: Text(
-                        "Giriş Yap",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyLarge
-                            ?.copyWith(color: ProjectColors.white),
-                        textAlign: TextAlign.center,
-                        textScaleFactor: ScaleSize.textScaleFactor(context),
+              // Text(ref.watch(loginProvider).toString()),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Firma Id',
+                        hintText: '',
                       ),
                     ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: ProjectColors.buttonGrey,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
+                    context.emptySizedHeightBoxLow,
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Parola',
+                        hintText: '',
+                      ),
                     ),
-                    //
-                  ),
+                    context.emptySizedHeightBoxNormal,
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => HomeView()));
+                      },
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: context.dynamicHeight(0.07),
+                        child: Center(
+                          child: Text(
+                            "Giriş Yap",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge
+                                ?.copyWith(color: Colors.white, fontSize: 20),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: ProjectColors.buttonGrey,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        //
+                      ),
+                    ),
+                  ],
                 ),
               ),
+              Expanded(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Şifremi Unuttum",
+                        style: TextStyle(color: ProjectColors.buttonGrey),
+                      ),
+                    ),
+                  ))
             ],
           ),
         ),
